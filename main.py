0342691ad82ef34361c293d2b8255660e50529be
orrda <orr.david@mail.huji.ac.il>
2024-03-24 13:11:39 +0200

    ''''''

--- main.py
+++ main.py
@@ -53,10 +53,9 @@ arr = np.array([-1.1, -1.05, -1, -0.95, -0.9])
 arr = arr*rydberg_energy
 for e in arr:
     U_array = problem.get_U(e)
-    plt.plot(problem.r_array, U_array)
+    normalized_U = U_array/sum(np.abs(U_array))
+    plt.plot(problem.r_array, normalized_U)
 
-
-plt.plot(problem.r_array, U_array)
 
 plt.xlabel('r [fm]')
 plt.ylabel('psi(r)')
@@ -115,6 +114,40 @@ E_guess = - rydberg_energy
 
 for n in range(1, 4):
     potential = lambda r: -1*fine_structure_constant*hydrogen_charge*hbarc/r
+    for l in range(0, n):
+        problem = Problem(
+            potential,
+            angular_momentum = l,
+            max_R = bohr_radius*20*n,
+            precision = 20000,
+            effective_mass = reduced_mass,
+            h_bar=hbar,
+        )
+        E_guess = -rydberg_energy/n**2
+        root = problem.find_E(E_guess, tolerance=10**-6)
+        U_array = problem.get_U(root)
+        normalized_U = U_array/sum(np.abs(U_array))
+        plt.plot(problem.r_array, normalized_U)
+        eta = 1 - root/(-rydberg_energy/n**2)
+        avr = np.sqrt(problem.get_average(root))
+
+
+        print("n=",n,", l=", l,", energy=", root,", eta=", eta,", average r=", avr)
+
+plt.xlabel('r [fm]')
+plt.ylabel('psi(r)')
+plt.title('U(r) for different l')
+plt.grid(True)
+plt.legend(['n=1, l=0', 'n=2, l=0', 'n=2, l=1', 'n=3, l=0', 'n=3, l=1', 'n=3, l=2'])
+plt.show()
+
+# question 3.2.3
+
+V_0 = np.pi*(bohr_radius**3)*283*(10**-6)
+potential = lambda r: -1*fine_structure_constant*hydrogen_charge*hbarc/r + V_0*np.exp(-r**2/(2*hydrogen_nucleus_radius**2))/((np.sqrt(2*np.pi)*hydrogen_nucleus_radius)**3)
+
+
+for n in range(1,4):
     for l in range(0, n):
         problem = Problem(
             potential,
